9c3bb5b63df4b4c15839c5f3d24f37ded8db29a6
Izael Miranda junior <j.izael@escola.pr.gov.br>
2025-10-01 00:02:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+const canva = document.getElementById('gameCanva');
+const ctx = canva.getContext('2d');
+
+let jogador = {
+    x: 0,
+    y: 0,
+    largura: 50,
+    altura: 50,
+    velocidadeX: 0,
+    velocidadeY: 0,
+    gravidade: 0.3,
+    forcaDoPulo: 12,
+    pulando: false
+};
+
+let alvo = {
+    x: 0,
+    y: 0,
+    largura: 50,
+    altura: 50,
+    velocidadeX: 0,
+    velocidadeY: 0,
+    pulando: false
+};
+
+let imagemFundo = new Image();
+imagemFundo.src = 'backgroud.png';
+
+const imagemJogador = new image();
+imagemJogador.src = 'player.png';
+
+const imagemAlvo = new Image();
+imagemAlvo.src = 'patric.png';
+
+function redimensionarCanvas() {
+    canva.width = window.innerWidth;
+    canva.height = window.innerHeight;
+}
+
+window.addEventListener('resize', redimensionarCanvas);
+redimensionarCanvas();
+
+function limitarMovimento(obj) {
+    if (obj.x < 0) obj.x = 0
+    if (obj.x + obj.largura > canva.width) obj.x = canva.width - obj.largura;
+    if (obj.y < 0) obj.y = 0
+    if (obj.y + obj.altura > canva.height) obj.y = canva.height - obj.altura;
+}
+
+function pular() {
+    if (!jogador.pulando) {
+        jogador.velocidadeY = -jogador.forcaDoPulo;
+        jogador.pulando = true;
+    }
+}
+
+function atualizarPuloJogador() {
+    if(!jogador.pulando) {
+        jogador.velocidadeY += jogador.gravidade;
+        jogador.y += jogador.velocidadeY;
+        if (jogador.y + jogador.altura >= canva.height) {
+            jogador.y + canva.height - jogador.altura;
+            jogador.pulando = false;
+            jogador.velocidadeY = 0;
+        }
+    }
+}
+
+function atualizarFisicaAlvo() {
+    if (alvo.pulando) {
+        alvo.velocidadeY += jogador.gravidade;
+        alvo.x += alvo.velocidadeX;
+        alvo.y += alvo.velocidadeY;
+
+        if (alvo.y + alvo.altura >= canva.height) {
+            alvo.y = canva.height - alvo.altura;
+            alvo.pulando = false;
+            alvo.velocidadeX = 0;
+            alvo.velocidadeY = 0;
+        } else {
+            alvo.velocidadeX *= 0.95;
+        }
+    }
+}
+
+function chutar() {
+    let distX = alvo.x + alvo.largura / 2 - ( jogador.x + jogador.largura / 2);
+    let distY = alvo.y + alvo.altura / 2 - ( jogador.y + jogador.altura / 2);
+    let distancia = Math.sqrt(distX ** 2 + distY ** 2);
+
+    if (distancia < 150) {
+        alvo.velocidadeX = distX > 0 ? 20 : -20;
+        alvo.velocidadeY = -20;
+        alvo.pulando = true;
+    }
+}
+
+function desenhar() {
+    ctx.clearRect(0, 0, canva.width, canva.height);
+
+    ctx.drawImage(imagemFundo, 0, 0, canva.width, canva.height);
+
+    ctx.drawImage(imagemJogador, jogador.x, jogador.y, jogador.largura, jogador.altura);
+
+    ctx.drawImage(imagemAlvo, alvo.x, alvo.y, alvo.largura, alvo.altura);
+}
+
+function jogo() {
+    if (teclas['ArrowLeft']) jogador.x -= 3;
+    if (teclas['ArrowRight']) jogador.x += 3;
+    if (teclas['ArrowUp']) jogador.y -= 3;
+    if (teclas['ArrowDown']) jogador.y += 3;
+
+    if (teclas['  '] && !jogador.pulando) pular();
+    if (teclas['f']) chutar();
+}
+
+atualizarPuloJogador();
+atualizarFisicaAlvo();
+
+limitarMovimento(jogador);
+limitarMovimento(alvo);
+
+desenhar();
+
+requestAnimationFrame(jogo);
+
+let teclas = {}
+window.addEventListener('keydown', (e) => teclas[e.key] = true);
+window.addEventListener('keyup', (e) => teclas[e.key] = false);
+
+let imagensCarregadas = 0;
+const totalImagens = 3;
+
+function verificarImagensCarregadas() {
+    imagensCarregadas++;
+    if(imagensCarregadas === totalImagens) {
+            jogo();
+    }
+}
+
+imagemJogador.onload = verificarImagensCarregadas;
+imagemAlvo.onload = verificarImagensCarregadas;
+imagemFundo.onload = verificarImagensCarregadas;
